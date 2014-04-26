87e48affa0876ca25747d9858b78c6b6226d171a
Zing <xianyuhuanyue@gmail.com>
2014-04-26 19:03:56 +0800

    nothing changed

--- src/com/example/myappdemo/Utils/GenerateXML.java
+++ src/com/example/myappdemo/Utils/GenerateXML.java
@@ -1,20 +1,18 @@
 package com.example.myappdemo.Utils;
 
-import com.example.myappdemo.R;
-
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
+
+import com.example.myappdemo.R;
 
 public class GenerateXML {
 	public static int genLinearLayoutItems(LinearLayout linearLayout, SimpleAdapter simpleAdapter, Context context) {
 		int count = simpleAdapter.getCount();
 		for(int i = 0; i < count; i++) {
 			View v = simpleAdapter.getView(i, null, linearLayout);
-//			context.getResources().getDrawable(R.drawable.home_divider)
 			//set different bg resource
 			if(i == 0){
 				v.setBackgroundResource(R.drawable.call_item_top_n);
