2623d63b05e87a7d67220ec47c55459891c01db0
Zing <xianyuhuanyue@gmail.com>
2014-04-28 01:00:10 +0800

    更改了颜色 去掉了主页面的titlebai

--- src/com/example/myappdemo/MainActivity.java
+++ src/com/example/myappdemo/MainActivity.java
@@ -11,8 +11,8 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.example.myappdemo.Utils.GenerateXML;
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		init();
 	}
