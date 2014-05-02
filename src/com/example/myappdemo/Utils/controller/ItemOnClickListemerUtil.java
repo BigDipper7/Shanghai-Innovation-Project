5937561a3dac29b59655b5fb5157f01d1bad768b
Zing <xianyuhuanyue@gmail.com>
2014-05-02 17:01:18 +0800

    最终可以成功一种案例 实现了最初始的page2的按键的监听

--- src/com/example/myappdemo/Utils/controller/ItemOnClickListemerUtil.java
+++ src/com/example/myappdemo/Utils/controller/ItemOnClickListemerUtil.java
@@ -1,16 +1,21 @@
 package com.example.myappdemo.utils.controller;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.example.myappdemo.R;
-import com.example.myappdemo.utils.view.CustomDialog;
-import com.example.myappdemo.utils.view.CustomDialog.OnCusDiaItmClickListener;
 
 public class ItemOnClickListemerUtil {
-	public static void setOnClickListener(final Context context,final View view, final int i) {
+	public static void setOnClickListener( final Context context, final View view,  final int i) {
 		view.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -28,23 +33,40 @@ public class ItemOnClickListemerUtil {
 				case 6: break;
 				default: break;
 				}
-				CustomDialog customDialog = new CustomDialog(context, resId, R.style.Page2CustomDialog, new OnCusDiaItmClickListener() {
+				
+				final View dialogContent = LayoutInflater.from(context).inflate(resId, null);
+				RadioGroup group = (RadioGroup) dialogContent.findViewById(R.id.radioGroup);
+				group.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 					
 					@Override
-					public void CusDiaItmClickListener(String string) {
-						//不清楚是V还是View
+					public void onCheckedChanged(RadioGroup arg0, int arg1) {
+						//获取变更后的选中项的ID
+						int radioButtonId = arg0.getCheckedRadioButtonId();
+						//根据ID获取RadioButton的实例
+						RadioButton rb = (RadioButton)dialogContent.findViewById(radioButtonId);
+						//更新文本内容，以符合选中项
+						String str = (String) rb.getText();
+						Log.e("---> CustomDialog - TextSlected", str);
 						TextView tv = (TextView) view.findViewById(R.id.textView2);
-						tv.setText(string);
+						tv.setText(str);
 					}
 				});
-				customDialog.setCanceledOnTouchOutside(true);
-				customDialog.show();
-//				CustomDialog cd = new CustomDialog(this, R.layout.Dialogxml,
-//						theme, new hudiaoFun() {
-//							onitemchoose(string str) {
-//								view.findviewbyid(R.id.textview2).setText(str);
-//							}
-//						});
+				Dialog alertDialog = new AlertDialog.Builder(context).setView(dialogContent).create();
+				alertDialog.show();
+				
+//				CustomDialog customDialog = new CustomDialog(context, resId, R.style.Page2CustomDialog, new OnCusDiaItmClickListener() {
+//					
+//					@Override
+//					public void CusDiaItmClickListener(String string) {
+//						//不清楚是V还是View
+//						TextView tv = (TextView) view.findViewById(R.id.textView2);
+//						tv.setText(string);
+//					}
+//				});
+//				Log.e("--->onclicklistener", ""+i);
+//				customDialog.setCanceledOnTouchOutside(true);
+//				customDialog.show();
+
 
 			}
 		});
